0ff94ffd0e9da32fd8e758a791a4471cf6dae906
Kelley Crowell <1kelley.crowell@gmail.com>
2016-02-03 22:45:58 -0800

    first commit

---
+++ Recursive_Fib.rb
@@ -0,0 +1,18 @@
+def fibs(number)
+  seq = [1, 2]
+
+  (number-2).times do |nb|
+    seq << seq[-1] + seq[-2]
+  end
+
+  seq
+end
+
+def fibs_rec(number)
+  return [1] if number == 1
+  return [1, 2] if number == 2
+  fibs_rec(number - 1) << fibs_rec(number - 1)[-1] + fibs_rec(number - 1)[-2]
+end
+
+puts fibs(10).inspect
+puts fibs_rec(10).inspect
